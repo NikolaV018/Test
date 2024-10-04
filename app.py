421616027afb615e49f532b2cc7fc5f6e2b2df59
NikolaV018 <nikolavuckoic1997@hotmail.com>
2024-10-05 00:08:20 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,12 @@
+def dna_string (dna):
+    c = {"A" : "T", "T" : "A", "G" : "C", "C" : "G"}
+    return ''.join([c[base] for base in dna])
+
+
+#input
+dna = input("Input a DNA string: ")
+dna = dna.upper()
+stampa = dna_string(dna)
+print(stampa)
+
+
